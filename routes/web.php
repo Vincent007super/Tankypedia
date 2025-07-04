6217fc2961a898555af5ac16c445d9e7b7f7127e
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-07-04 13:56:51 +0200

    Resolve merge conflicts in composer.json and web.php
    Cleaned up merge conflict markers in composer.json, composer.lock, and routes/web.php. Updated the root route to return a custom HTML message instead of the default welcome view.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< Updated upstream
-    return view('welcome');
-=======
     return "<h1 style='font-size:15vw'> Yo whatsup mother cluckers! </h1>";
->>>>>>> Stashed changes
 });
