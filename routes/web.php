e4cc4699eb3a18cf0766618b584039cc7d5a4f1e
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-07-04 13:46:39 +0200

    Initial commit
    Set up project structure and add initial files.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+<<<<<<< Updated upstream
     return view('welcome');
+=======
+    return "<h1 style='font-size:15vw'> Yo whatsup mother cluckers! </h1>";
+>>>>>>> Stashed changes
 });
